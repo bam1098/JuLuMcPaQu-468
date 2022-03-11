353f69d0544b9eb522af7507832054fb792fa295
Devin Parentice <devinparentice@gmail.com>
2022-03-11 01:48:33 -0500

    Fixed profile.py

--- profile.py
+++ profile.py
@@ -20,36 +20,27 @@ request.addTour(tour)
 prefixForIP = "192.168.1."
 link = request.LAN("lan")
 
-num_nodes = 3
-for i in range(num_nodes):
-    if i == 0:
-        node = request.XenVM("head")
-        bs_landing = node.Blockstore("bs_image", "/image")
-        bs_landing.size = "500GB"
-    else:
-        node = request.XenVM("worker-" + str(i))
-    node.cores = 4
-    node.ram = 8192
-    node.routable_control_ip = "true"
-    node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-    iface = node.addInterface("if" + str(i))
-    iface.component_id = "eth1"
-    iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
-    link.addInterface(iface)
+node = request.XenVM("main")
+bs_landing = node.Blockstore("bs_image", "/image")
+bs_landing.size = "500GB"
+node.cores = 4
+node.ram = 8192
+node.routable_control_ip = "true"
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+iface = node.addInterface("if" + str(1))
+iface.component_id = "eth1"
+iface.addAddress(pg.IPv4Address(prefixForIP + str(1), "255.255.255.0"))
+link.addInterface(iface)
 
-    # setup Docker
-    node.addService(pg.Execute(
-        shell="sh", command="sudo bash /local/repository/install_docker.sh"))
-    # setup Kubernetes
-    node.addService(pg.Execute(
-        shell="sh", command="sudo bash /local/repository/install_kubernetes.sh"))
-    node.addService(pg.Execute(shell="sh", command="sudo swapoff -a"))
+# setup Docker
+node.addService(pg.Execute(
+    shell="sh", command="sudo bash /local/repository/install_docker.sh"))
+# setup Kubernetes
+node.addService(pg.Execute(
+    shell="sh", command="sudo bash /local/repository/install_kubernetes.sh"))
+node.addService(pg.Execute(shell="sh", command="sudo swapoff -a"))
 
-    if i == 0:
-        node.addService(pg.Execute(
-            shell="sh", command="sudo bash /local/repository/kube_manager.sh"))
-    else:
-        node.addService(pg.Execute(
-            shell="sh", command="sudo bash /local/repository/kube_worker.sh"))
+node.addService(pg.Execute(
+    shell="sh", command="sudo bash /local/repository/kube_manager.sh"))
 
 pc.printRequestRSpec(request)
