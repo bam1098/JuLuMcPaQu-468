d07c0fc6f5b6712f947e3844de672b3e89a09972
EmiQuin <78883350+EmiQuin@users.noreply.github.com>
2022-04-14 12:52:48 -0400

    Delete backend directory

--- backend/routes/profile.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getPrivateData } = require("../controllers/profile");
-const { protect } = require("../middleware/auth");
-
-router.route("/").get(protect, getPrivateData);
-
-module.exports = router;
